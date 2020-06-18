06daf9623063af9870d0f428d74b220389c18929
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-06-18 14:18:19 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,8 +4,8 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      <h1>Actors Page</h1>
-      {actors.map(actor => <div>Name: {actor.name} <ul>{actor.movies.map()movie = <li>{movie}</li>)}</ul></div>)}
+    <h1>Actors Page</h1>
+    {actors.map(actor => <div>Name: {actor.name} <ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul></div>)}
     </div>
   );
 };
